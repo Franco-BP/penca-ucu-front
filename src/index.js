6d14957be1815bf21aaacb46d0bd4c76a18c8805
Giorgio <Giorgio.san007@gmail.com>
2024-06-17 17:22:16 -0300

    Se implementa distintos tipos de contexto, para dividir por funcionalidad

--- src/index.js
+++ src/index.js
@@ -10,8 +10,6 @@ const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
     <App />
-    <Layout />
-    <MatchCarousel />
   </React.StrictMode>
 );
 
