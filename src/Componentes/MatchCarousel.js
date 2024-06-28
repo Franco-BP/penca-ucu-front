4aa048ef730d8609c2beb0207a7b0e9c7fe5327b
Enzo <enzuii@hotmail.com>
2024-06-28 05:01:53 -0300

    cambios

--- src/Componentes/MatchCarousel.js
+++ src/Componentes/MatchCarousel.js
@@ -2,7 +2,7 @@ import React, { useEffect, useState, useContext } from 'react';
 import Carousel from 'react-material-ui-carousel';
 import MatchCard from './MatchCard';
 import { Box } from '@mui/material';
-import { PencaUCUContext, accionGetFuturePartidoData, accionSetSelectedPartido } from '../context/context';
+import { PencaUCUContext, accionSetSelectedPartido } from '../context/context';
 import { getWithResponseManage } from '../services/PencaUCUservices';
 
 const MatchCarousel = () => {
@@ -10,19 +10,21 @@ const MatchCarousel = () => {
   const [groupedMatches, setGroupedMatches] = useState([]);
 
   useEffect(() => {
-    // Setup the request payload
-    const today = new Date();
-    const dateString = today.toISOString(); // Format as ISO string (e.g., "2022-06-12T03:00:00.000Z")
-
-    const requestPayload = {
-      fecha: "2024-06-28 00:00:00.000000",
-      idTorneo: 9
-    };
-
-    getWithResponseManage('/partido/getAll', requestPayload)
+    getWithResponseManage('/partido/getAll')
       .then((response) => {
-        dispatch(accionGetFuturePartidoData(response));
-        console.log('Future partido data:', response); 
+        const now = new Date();
+        now.setHours(now.getHours() - 1);
+
+        const futureMatches = response.filter(partido => new Date(partido.fecha) > now);
+        const groupMatches = (matches, groupSize) => {
+          const matchGroups = [];
+          for (let i = 0; i < matches.length; i += groupSize) {
+            matchGroups.push(matches.slice(i, i + groupSize));
+          }
+          return matchGroups;
+        };
+
+        setGroupedMatches(groupMatches(futureMatches, 3));
       })
       .catch(error => console.log(error));
   }, [dispatch]);
@@ -30,20 +32,6 @@ const MatchCarousel = () => {
   const handleSelectPartido = (partido) => {
     dispatch(accionSetSelectedPartido(partido));
   };
-
-  useEffect(() => {
-    if (data.futurePartidoData) {
-      const groupMatches = (matches, groupSize) => {
-        const matchGroups = [];
-        for (let i = 0; i < matches.length; i += groupSize) {
-          matchGroups.push(matches.slice(i, i + groupSize));
-        }
-        return matchGroups;
-      };
-
-      setGroupedMatches(groupMatches(data.futurePartidoData, 3));
-    }
-  }, [data.futurePartidoData]);
 
   return (
     <Box sx={{ width: '100%' }}>
